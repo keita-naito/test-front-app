cb1eb6ee0d92dc624c17e7bc565a2537753612b0
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-19 02:11:35 +0900

    開発の各種設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  plugins: [
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/html-closing-bracket-newline': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }
