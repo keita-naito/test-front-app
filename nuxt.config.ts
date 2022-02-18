0e4feab735b77665be5e18c069ed45b9221cf1e8
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-19 02:31:18 +0900

    vuetify を使えるように設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,8 +27,7 @@ const config: NuxtConfig = {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
+  plugins: [{ src: '~/plugins/vuetify.js', mode: 'client' }],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
