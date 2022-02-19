579834c76c051c0163f7329bee35014f89bd1cde
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-20 02:26:26 +0900

    仮の動画再生ページを実装

---
+++ app/pages/movie.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div>動画ページ</div>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent, useContext, useAsync } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    const {} = useContext()
+
+    useAsync(async () => {})
+
+    return {}
+  },
+})
+</script>
