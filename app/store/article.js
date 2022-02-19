1d940d80d0342e8f2f5980ab50bcf8deebdb7899
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-19 18:51:06 +0900

    記事に関する store ファイルを追加

---
+++ app/store/article.js
@@ -0,0 +1,25 @@
+import camelcaseKeys from 'camelcase-keys'
+
+export const state = () => ({
+  articles: {},
+})
+
+export const getters = {
+  articles: (state) => state.articles,
+}
+
+export const mutations = {
+  setArticles(state, articles) {
+    console.log(articles)
+    state.articles = articles
+  },
+}
+
+export const actions = {
+  async fetchArticles({ commit }) {
+    const response = await this.$axios.get(`/articles`)
+
+    const data = camelcaseKeys(response.data?.data, { deep: true })
+    commit('setArticles', data)
+  },
+}
