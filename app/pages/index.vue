38f5224a189d98916deb1343090202be6ac127a7
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-19 19:17:38 +0900

    記事一覧が表示されるように調整

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,39 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div>トップページだよ</div>
+      <div v-for="article in articles" :key="article.id">
+        <div>{{ article.content }}</div>
+      </div>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  useContext,
+  useAsync,
+  computed,
+} from '@nuxtjs/composition-api'
 export default defineComponent({
-  setup() {},
+  setup() {
+    const { store } = useContext()
+
+    useAsync(async () => {
+      try {
+        await store.dispatch('article/fetchArticles')
+      } catch (err) {
+        console.log(err)
+      }
+    })
+
+    const articles = computed(() => {
+      return store.getters['article/articles']
+    })
+
+    return {
+      articles,
+    }
+  },
 })
 </script>
